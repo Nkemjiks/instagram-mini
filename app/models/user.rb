b1413699cb9f27d43dcee33fc273eeb0eca06f45
Blessing Mbonu <mbonunkemjika@ymail.com>
2020-05-11 17:16:09 +0100

    User model and navbar

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  has_one_attached :avatar
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  validates :first_name, presence: true, length: { minimum:3 }
+  validates :last_name, presence: true, length: { minimum:3 }
+end
