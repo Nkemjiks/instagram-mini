385b986afa4b25352518c0454cdc725034f7b9a9
Blessing Mbonu <mbonunkemjika@ymail.com>
2020-05-13 08:16:20 +0100

    changes to how i use npm packages

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,9 @@ require("@rails/activestorage").start()
 require("channels")
 require('bootstrap')
 
+import '../css/main.scss'
+import "@fortawesome/fontawesome-free/js/all";
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
